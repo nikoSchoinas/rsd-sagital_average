ef4e1fee03e01c35dc1db78adc60e8b0c8b97493
Nikolaos Schoinas <dai16167@uom.edu.gr>
2020-11-26 17:43:05 +0000

    Create an entry point to the package.

---
+++ sagital_average/command.py
@@ -0,0 +1,13 @@
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+from .sagital_brain import run_averages
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description="Calculates the average for each sagital-horizontal plane.",
+                            formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument('file_input', nargs='?', default="brain_sample.csv",
+                        help="Input CSV file with the results from scikit-brain binning algorithm.")
+    parser.add_argument('--file_output', '-o', default="brain_average.csv",
+                        help="Name of the output CSV file.")
+    arguments = parser.parse_args()
+
+    run_averages(arguments.file_input, arguments.file_output)
--- setup.py
+++ setup.py
@@ -4,4 +4,9 @@ setup(
     name="sagital_average",
     version="0.1.0",
     packages=find_packages(),
+    entry_points = {
+        'console_scripts' : [
+            'run_averages = sagital_brain.command:process' 
+        ]
+    }
 )
